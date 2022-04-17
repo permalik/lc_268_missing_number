e4948280c18c0838aac58cd11ce41b802c532a39
Malik <permalik@protonmail.com>
2022-04-17 10:08:55 -0500

    Complete alternative solution

--- 268_missing_number.ts
+++ 268_missing_number.ts
@@ -39,6 +39,12 @@ const missingNumber = function (array: any) {
   let solution = sumOfRange - sumOfOriginalArray;
 
   console.log(solution);
+
+  // nums.sort((a, b) => a - b);
+  // for (let i = 0; i < nums.length; i++) {
+  //   if (i !== nums[i]) return i;
+  // }
+  // return nums[nums.length - 1] + 1;
 };
 
 let nums = [9, 6, 4, 2, 3, 5, 7, 0, 1];
